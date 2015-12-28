730e3c583ba167ee5cc75c207a5d11f58e524d1b
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2015-12-28 00:04:53 -0800

    extended test

--- test/basic.js
+++ test/basic.js
@@ -3,9 +3,44 @@
 var lib = require('../lib'),
     expect = require('chai').expect;
 
+var dat = {
+    bold: {
+        src: 'aaa**bold1**aaa',
+        dst: ''
+    },
+    italic: {
+        src: 'aaa*italic1*aaa',
+        dst: ''
+    },
+    subscript: {
+        src: 'aaa<sub>subscript</sub>aaa',
+        dst: ''
+    },
+    superscript: {
+        src: 'aaa<sup>superscript</sup>aaa',
+        dst: ''
+    },
+    strikethrough: {
+        src: 'aaa<s>strikethrough</s>aaa',
+        dst: ''
+    },
+    overline: {
+        src: 'aaa<o>overline</o>aaa',
+        dst: ''
+    },
+    underline: {
+        src: 'aaa<u>underline</u>aaa',
+        dst: ''
+    }
+}
+
 describe('basic', function () {
-    it('t0', function (done) {
-        expect(lib.parse('abc<o>def</o>gh')).to.eq(['abc', 'def', 'gh']);
-        done();
+    Object.keys(dat).forEach(function (key) {
+        it(key, function (done) {
+            var src = dat[key].src;
+            var dst = dat[key].dst;
+            expect(lib.parse(src)).to.eq(dst);
+            done();
+        });
     });
 });
